06891cb740b524140b7a00baf64f1ed73e204d05
Harsh Shah <hshah@itbit.com>
2023-12-16 01:02:08 +0530

    init

--- contracts/AccessRegistryOracle.sol
+++ contracts/AccessRegistryOracle.sol
@@ -3,59 +3,87 @@ pragma solidity 0.8.17;
 
 import {Authorizable} from "./Authorizable.sol";
 
-/*
- * Listing all the TODO:
- *   1. Add events.
- *   2. Add comments.
- *   3. Behavior, if you add/remove duplicate, its a no-op.
+/**
+ * @title AccessRegistry: Manage a list of address to allow and block them.
  */
-
 contract AccessRegistryOracle is Authorizable {
     // DATA
     mapping(address => bool) internal allowList;
     mapping(address => bool) internal blockList;
-
     // DATA ENDS
 
+    // Events
+    event ModifiedAllowList(address indexed _addr, string _action);
+    event ModifiedBlockList(address indexed _addr, string _action);
+
+    /**
+     * @notice Add to allow list of wallet addresses.
+     * @param _toAddAddresses address[] This is the list of addresses to add to allow list.
+     */
     function addToAllowList(
         address[] calldata _toAddAddresses
     ) external onlyAuthorized {
         for (uint i = 0; i < _toAddAddresses.length; i++) {
             allowList[_toAddAddresses[i]] = true;
+            emit ModifiedAllowList(_toAddAddresses[i], "Add");
         }
     }
 
+    /**
+     * @notice Remove from allow list of wallet addresses.
+     * @param _toAddAddresses address[] This is the list of addresses to remove from allow list.
+     */
     function removeFromAllowList(
         address[] calldata _toAddAddresses
     ) external onlyAuthorized {
         for (uint i = 0; i < _toAddAddresses.length; i++) {
             delete allowList[_toAddAddresses[i]];
+            emit ModifiedAllowList(_toAddAddresses[i], "Remove");
         }
     }
 
+    /**
+     * @notice Add to block list of wallet addresses.
+     * @param _toAddAddresses address[] This is the list of addresses to add to block list.
+     */
     function addToBlockList(
         address[] calldata _toAddAddresses
     ) external onlyAuthorized {
         for (uint i = 0; i < _toAddAddresses.length; i++) {
             blockList[_toAddAddresses[i]] = true;
+            emit ModifiedBlockList(_toAddAddresses[i], "Add");
         }
     }
 
+    /**
+     * @notice Remove from block list of wallet addresses.
+     * @param _toAddAddresses address[] This is the list of addresses to remove from block list.
+     */
     function removeFromBlockList(
         address[] calldata _toAddAddresses
     ) external onlyAuthorized {
         for (uint i = 0; i < _toAddAddresses.length; i++) {
             delete blockList[_toAddAddresses[i]];
+            emit ModifiedBlockList(_toAddAddresses[i], "Remove");
         }
     }
 
-    // Check functions
+    /**
+     * @notice Check if address is part of allow list.
+     * @param _addr address Check if this address is part of allow list.
+     * @return bool True if address is part of allow list, false otherwise.
+     */
     function isAllowed(
         address _addr
     ) external view onlyAuthorized returns (bool) {
         return allowList[_addr];
     }
 
+    /**
+     * @notice Check if address is part of block list.
+     * @param _addr address Check if this address is part of block list.
+     * @return bool True if address is part of block list, false otherwise.
+     */
     function isBlocked(
         address _addr
     ) external view onlyAuthorized returns (bool) {
--- contracts/Authorizable.sol
+++ contracts/Authorizable.sol
@@ -7,24 +7,46 @@ error ZerrorAddress();
 error NotAuthorized();
 
 contract Authorizable is Ownable {
+    // Mapping of address which are authorized.
     mapping(address => bool) public authorized;
+
+    // Events
+    event Authorized(address indexed _addr);
+    event UnAuthorized(address indexed _addr);
 
     modifier onlyAuthorized() {
         if(!(authorized[msg.sender] || owner() == msg.sender)) revert NotAuthorized();
         _;
     }
 
+    /**
+     * @notice Add an address to the authorized list.
+     * @param _toAdd address The address to add to the authorized list.
+     * @return bool True if the address was added to the authorized list.
+     */
     function addAuthorization(address _toAdd) public onlyOwner returns (bool){
         if (_toAdd == address(0)) revert ZerrorAddress();
         authorized[_toAdd] = true;
+        emit Authorized(_toAdd);
         return true;
     }
 
+    /**
+     * @notice Remove an address from the authorized list.
+     * @param _toRemove address The address to remove from the authorized list.
+     * @return bool True if the address was remove from the authorized list.
+     */
     function removeAuthorization(address _toRemove) public onlyOwner returns (bool){
         delete authorized[_toRemove];
+        emit UnAuthorized(_toRemove);
         return true;
     }
 
+    /**
+     * @notice Add an address to the authorized list.
+     * @param _addr address The address to add to the authorized list.
+     * @return bool True if address is part of authorized list, False otherwise.
+     */
     function isAuthorized(address _addr) public view onlyOwner returns (bool) {
         return authorized[_addr];
     }
